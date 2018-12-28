0f4c779044ce872bf0c11602e041391359c85297
sunfu <sunfu@shuidihuzhu.com>
2018-12-28 18:32:03 +0800

    init

--- hbase-transfer/src/main/java/com/shuidihuzhu/transfer/Application.java
+++ hbase-transfer/src/main/java/com/shuidihuzhu/transfer/Application.java
@@ -1,23 +1,79 @@
 package com.shuidihuzhu.transfer;
 
+import com.ngdata.sep.EventListener;
+import com.ngdata.sep.PayloadExtractor;
+import com.ngdata.sep.SepEvent;
+import com.ngdata.sep.SepModel;
+import com.ngdata.sep.impl.BasePayloadExtractor;
+import com.ngdata.sep.impl.SepConsumer;
+import com.ngdata.sep.impl.SepModelImpl;
+import com.ngdata.sep.util.zookeeper.ZkUtil;
+import com.ngdata.sep.util.zookeeper.ZooKeeperItf;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
+
+import java.util.List;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class
-})
+@EnableAutoConfiguration
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = {"com.shuidihuzhu.transfer", "com.shuidihuzhu.data"})
-@ComponentScan(basePackages = {"com.shuidihuzhu.client.dataservice.ds.v1","com.shuidihuzhu.baseservice.hawkeye", "com.shuidihuzhu.data"})
 public class Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
+
+        /*try {
+            Configuration conf = HBaseConfiguration.create();
+            conf.setBoolean("hbase.replication", true);
+
+            ZooKeeperItf zk = ZkUtil.connect("localhost", 20000);
+            SepModel sepModel = new SepModelImpl(zk, conf);
+
+            final String subscriptionName = "logger";
+
+            if (!sepModel.hasSubscription(subscriptionName)) {
+                sepModel.addSubscriptionSilent(subscriptionName);
+            }
+
+            PayloadExtractor payloadExtractor = new BasePayloadExtractor(Bytes.toBytes("sep-user-demo"), Bytes.toBytes("info"),
+                    Bytes.toBytes("payload"));
+
+            SepConsumer sepConsumer = new SepConsumer(subscriptionName, 0, new EventLogger(), 1, "localhost", zk, conf,
+                    payloadExtractor);
+
+            sepConsumer.start();
+            System.out.println("Started");
+
+            while (true) {
+                Thread.sleep(Long.MAX_VALUE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
     }
+
+
+    /*private static class EventLogger implements EventListener {
+        @Override
+        public void processEvents(List<SepEvent> sepEvents) {
+            for (SepEvent sepEvent : sepEvents) {
+                System.out.println("Received event:");
+                System.out.println("  table = " + Bytes.toString(sepEvent.getTable()));
+                System.out.println("  row = " + Bytes.toString(sepEvent.getRow()));
+                System.out.println("  payload = " + Bytes.toString(sepEvent.getPayload()));
+                System.out.println("  key values = ");
+                for (Cell kv : sepEvent.getKeyValues()) {
+                    System.out.println("    " + kv.toString());
+                }
+            }
+        }
+    }*/
 }
