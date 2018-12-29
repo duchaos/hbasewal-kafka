e4ff56bb172338cde367dc22c8bcadb04fc7d4f9
sunfu <sunfu@shuidihuzhu.com>
2018-12-29 14:56:42 +0800

    init

--- hbase-transfer/src/main/java/com/shuidihuzhu/transfer/Application.java
+++ hbase-transfer/src/main/java/com/shuidihuzhu/transfer/Application.java
@@ -1,9 +1,7 @@
 package com.shuidihuzhu.transfer;
 
 
-import com.ngdata.sep.EventListener;
 import com.ngdata.sep.PayloadExtractor;
-import com.ngdata.sep.SepEvent;
 import com.ngdata.sep.SepModel;
 import com.ngdata.sep.impl.BasePayloadExtractor;
 import com.ngdata.sep.impl.SepConsumer;
@@ -12,23 +10,20 @@ import com.ngdata.sep.util.zookeeper.ZkUtil;
 import com.ngdata.sep.util.zookeeper.ZooKeeperItf;
 import com.shuidihuzhu.transfer.listener.SepEventListener;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.Cell;
-import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages = {
-        "com.shuidihuzhu.transfer"
-})
+@SpringBootApplication(scanBasePackages = {"com.shuidihuzhu.transfer"})
 public class Application {
 
     static final String zookeeperConnectors = "localhost";
     static final String subscriptionName = "logger";
     static final String hbaseTable = "sep-user-demo";
     static final String hbaseColumnFamily = "info";
+    static final String columnQualifier = "payload";
 
 
 
@@ -36,31 +31,27 @@ public class Application {
 
         // todo 配置接入configserver
 
-
-
-
-
         try {
+            // 连接zk
+            ZooKeeperItf zk = ZkUtil.connect(zookeeperConnectors, 20000);
+
+            // 访问zk，add peers
             Configuration conf = HBaseConfiguration.create();
             conf.setBoolean("hbase.replication", true);
-
-            ZooKeeperItf zk = ZkUtil.connect(zookeeperConnectors, 20000);
             SepModel sepModel = new SepModelImpl(zk, conf);
-
             if (!sepModel.hasSubscription(subscriptionName)) {
                 sepModel.addSubscriptionSilent(subscriptionName);
             }
 
-            PayloadExtractor payloadExtractor = new BasePayloadExtractor(Bytes.toBytes(hbaseTable), Bytes.toBytes(hbaseColumnFamily), Bytes.toBytes("payload"));
+            // 什么鬼？
+            PayloadExtractor payloadExtractor = new BasePayloadExtractor(Bytes.toBytes(hbaseTable), Bytes.toBytes(hbaseColumnFamily), Bytes.toBytes(columnQualifier));
 
+            // 创建sep rpc server
             SepConsumer sepConsumer = new SepConsumer(subscriptionName, 0, new SepEventListener(), 1, "localhost", zk, conf, payloadExtractor);
-
             sepConsumer.start();
-            System.out.println("Started");
 
-            /*while (true) {
-                Thread.sleep(Long.MAX_VALUE);
-            }*/
+            System.out.println("hbase transfer started...");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
