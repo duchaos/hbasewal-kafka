e78e6bd8098bffb526b39f8611f0e157d5306c05
sunfu <sunfu@shuidihuzhu.com>
2019-01-06 14:14:04 +0800

    init

--- hbase-transfer/src/main/java/com/shuidihuzhu/transfer/model/SinkRecord.java
+++ hbase-transfer/src/main/java/com/shuidihuzhu/transfer/model/SinkRecord.java
@@ -119,7 +119,8 @@ public class SinkRecord {
                 //record.getFamily() + "|" +
                 //record.getQualifier() + "|" +
                 record.getColumn() + "|" +
-                record.getValue() + "|" +
+                //record.getValue() + "|" +
+                record.getKeyValues() + "|" +
                 record.getTimestamp() + "|" +
                 record.getPayload();
     }
