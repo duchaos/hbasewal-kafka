3637d1706bffde091dafa115d72d4b967eb2e17f
王伟 <wangwei@shuidihuzhu.com>
2019-01-06 15:38:03 +0800

    es batch update

--- hbase-transfer/src/main/java/com/shuidihuzhu/transfer/sink/ESSink.java
+++ hbase-transfer/src/main/java/com/shuidihuzhu/transfer/sink/ESSink.java
@@ -10,26 +10,14 @@ import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.JestResult;
 import io.searchbox.client.config.HttpClientConfig;
-import io.searchbox.core.Update;
 import io.searchbox.core.UpdateByQuery;
 import org.apache.commons.lang.StringUtils;
-import org.apache.hadoop.hbase.Cell;
-import org.apache.lucene.document.Document;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.ElasticsearchClient;
-import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.reindex.UpdateByQueryAction;
-import org.elasticsearch.index.reindex.UpdateByQueryRequestBuilder;
-import org.elasticsearch.index.search.SimpleQueryStringQueryParser;
-import org.elasticsearch.script.Script;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.net.InetAddress;
 import java.util.List;
 import java.util.Map;
 
@@ -66,15 +54,6 @@ public class ESSink extends AbstractSink implements InitializingBean {
         SearchSourceBuilder sourceBuilder = null;
         JestResult result = null;
         try{
-//            String fieldName = "field";
-//            sourceBuilder = buildSearch(record,fieldName);
-//            String qslStr = sourceBuilder.toString();
-//            JSONObject scriptObject = JSON.parseObject(qslStr);
-//            JSONObject updateByQueryJson = new JSONObject();
-//            scriptObject.getJSONObject("query");
-//
-//            updateByQueryJson.put("query",scriptObject.getJSONObject("query"));
-//            updateByQueryJson.put("script",scriptObject.getJSONObject("script_fields").getJSONObject(fieldName).getJSONObject("script"));
             UpdateByQuery updateByQuery = new UpdateByQuery.Builder(buildSearch(record))
                     .addIndex("sdhz_user_info_realtime")
                     .addType("detail")
@@ -103,7 +82,11 @@ public class ESSink extends AbstractSink implements InitializingBean {
         }
         JSONObject scriptObject = JSON.parseObject(searchSourceBuilder.toString());
         Map<String,String> scriptMap = Maps.newHashMap();
-        scriptMap.put("source","ctx._source."+record.getFamily()+"_"+record.getQualifier()+"='"+record.getValue()+"'");
+        String script = "";
+        for(Map.Entry<String,Object> tmp : record.getKeyValues().entrySet()){
+            script = script+"ctx._source."+tmp.getKey()+"='"+record.getValue()+"';";
+        }
+        scriptMap.put("source",script);
         scriptMap.put("lang","painless");
         scriptObject.put("script", scriptMap);
         return scriptObject.toString();
